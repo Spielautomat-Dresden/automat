682acf45348705f8e0a10e1c71bc1ed654c41479
jonasbieber <mail@jonasbieber.de>
2018-03-03 17:34:33 +0100

    Timersteuerung für Abtastrate Versuch 2

--- Hauptsteuerung/Lochdetektion/Lochdetektion.ino
+++ Hauptsteuerung/Lochdetektion/Lochdetektion.ino
@@ -1,1 +1,26 @@
+/*
+Startet Timer 3. Dieser dient dem Einstellen einer konstanten Abtastrate.
+*/
+void starteAbtastTimer(int abtastPeriode_ms)
+{
+  TCCR3A = 0x0000; // Keine Pins verbunden
+  TCCR3B = (1 << WGM33) | (1 << WGM32); // CTC Mode mit ICR3 als Top
+  TCCR3B |= (1 << CS30); // Prescaler 1 -> ohne Prescaler
+  int prescaler = 1;
+  ICR3 = abtastPeriode_ms*((F_CPU)/prescaler)/1000; // Input Capture Register
+}
 
+void setup()
+{
+  starteAbtastTimer(4);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  
+  
+  while( ! (TIFR3 & (1<<ICF4)) );
+  TIFR3 = 1<<ICF4;
+}
